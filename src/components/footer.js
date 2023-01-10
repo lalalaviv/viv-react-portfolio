5e26061a12916beaa1889e55b8eb3814b1f0c006
lalalaviv <lalala.viv@hotmail.com>
2023-01-11 03:44:36 +0800

    corrected path of icon display

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
-import {ReactComponent as GITHUB} from '../../Resources/icons/github.svg'
-import {ReactComponent as INSTAGRAM} from '../../Resources/icons/instagram.svg'
-import {ReactComponent as LINKEDIN} from '../../Resources/icons/linkedin.svg'
+import {ReactComponent as GITHUB} from '../Resources/icons/github.svg'
+import {ReactComponent as INSTAGRAM} from '../Resources/icons/instagram.svg'
+import {ReactComponent as LINKEDIN} from '../Resources/icons/linkedin.svg'
 
 const Footer =
 [
