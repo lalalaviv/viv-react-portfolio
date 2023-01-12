3d3df610e73ee468ad6d9b88eedca62344d3bdbf
lalalaviv <lalala.viv@hotmail.com>
2023-01-12 20:41:01 +0800

    Updates

---
+++ precache-manifest.cfd3bcd30e615dc84b32aad175417236.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89b622eabd480f2e9e445eef5c89f9ba",
+    "url": "/viv-react-portfolio.git/index.html"
+  },
+  {
+    "revision": "d1a5f588449b0b1b87e8",
+    "url": "/viv-react-portfolio.git/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "8bfa85b1abe8bece3a3b",
+    "url": "/viv-react-portfolio.git/static/css/main.b7bec8b5.chunk.css"
+  },
+  {
+    "revision": "d1a5f588449b0b1b87e8",
+    "url": "/viv-react-portfolio.git/static/js/2.e6c474ee.chunk.js"
+  },
+  {
+    "revision": "17addde56dada538fe1bc33d341e4ae4",
+    "url": "/viv-react-portfolio.git/static/js/2.e6c474ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bfa85b1abe8bece3a3b",
+    "url": "/viv-react-portfolio.git/static/js/main.2112d907.chunk.js"
+  },
+  {
+    "revision": "085c24680a0cbc538c4d",
+    "url": "/viv-react-portfolio.git/static/js/runtime-main.cf923a9b.js"
+  },
+  {
+    "revision": "319b3c1c591189cfddfce601a4b1ec56",
+    "url": "/viv-react-portfolio.git/static/media/2_lines.319b3c1c.woff"
+  },
+  {
+    "revision": "897a221672c8a3996290ac004e9fd80c",
+    "url": "/viv-react-portfolio.git/static/media/avatar.897a2216.jpg"
+  },
+  {
+    "revision": "761402ba4bf5fe3996d0f04723f7c51a",
+    "url": "/viv-react-portfolio.git/static/media/codeQuiz.761402ba.png"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/viv-react-portfolio.git/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "662a4f1472618eb2eb3b507d7459741f",
+    "url": "/viv-react-portfolio.git/static/media/instagram.662a4f14.svg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/viv-react-portfolio.git/static/media/linkedin.619231ff.svg"
+  },
+  {
+    "revision": "a3b80f2678a5207257de56bec692076a",
+    "url": "/viv-react-portfolio.git/static/media/noteTaker.a3b80f26.png"
+  },
+  {
+    "revision": "0d55b663b9ea28fb7bdf5b414ec7df0b",
+    "url": "/viv-react-portfolio.git/static/media/passwordGenerator.0d55b663.png"
+  },
+  {
+    "revision": "93b9742d0a00a0357dc04879d2994c1b",
+    "url": "/viv-react-portfolio.git/static/media/resume.93b9742d.pdf"
+  },
+  {
+    "revision": "4d1708ceb24970833fc0088f8f100cd2",
+    "url": "/viv-react-portfolio.git/static/media/theTraveller.4d1708ce.png"
+  },
+  {
+    "revision": "19f4f7c586f7b5ae49261c5fc6cad6bb",
+    "url": "/viv-react-portfolio.git/static/media/weatherDashboard.19f4f7c5.png"
+  },
+  {
+    "revision": "e0c210246f238b4e70bbfd7277793b30",
+    "url": "/viv-react-portfolio.git/static/media/workDayScheduler.e0c21024.png"
+  }
+]);
